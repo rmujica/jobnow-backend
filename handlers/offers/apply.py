9324c498ddddd61ad83cafd31854d82d34e98c61
René Mujica Moreau <rene.mujica.m@gmail.com>
2014-08-30 19:50:02 -0400

    add application

--- handlers/offers/apply.py
+++ handlers/offers/apply.py
@@ -13,25 +13,27 @@ class ApplyOfferHandler(RequestHandler):
         # is valid uid?
         try:
             offer_id = ObjectId(oid)
+            user_id = ObjectId(uid)
         except InvalidId:
             self.send_error(400)
             return
 
-        offer = yield db.offers.find_one({"_id": oid})
+        offer = yield db.offers.find_one({"_id": offer_id})
         if offer is None:
+            print("offer is: " + str(offer_id))
             self.send_error(404)
             return
-            
+
         # add application
         result = yield db.offers.update({
-            "_id": oid
+            "_id": offer_id
         }, {
             "$push": {
-                "candidates": uid
+                "candidates": user_id
             }
         })
 
-        updated_offer = yield db.offers.find_one({"_id": oid})
+        updated_offer = yield db.offers.find_one({"_id": offer_id})
         updated_offer["_id"] = str(updated_offer["_id"])
 
         self.set_status(201)
