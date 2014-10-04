30da21a244700d8fdd99cf46f53a6304f579160b
René Mujica Moreau <rene.mujica.m@gmail.com>
2014-10-04 16:54:13 -0300

    GET /offers/:id done

--- v2/handlers/offers/search.py
+++ v2/handlers/offers/search.py
@@ -11,53 +11,23 @@ import helpers.json as jsonhandler
 
 class SearchOfferHandler(RequestHandler):
     @gen.coroutine
-    def get(self, user):
-        search = self.get_query_argument("q", default=None)
-        uid = self.get_query_argument("u", default=None)
-        offers = list()
-        ret = dict()
+    def get(self, offer_id):
         db = self.settings["db"]
 
-        if search is None and uid is None:
-            # get all offers
-            ret["search_terms"] = list()
-            cursor = db.offers.find()
-            
-            while (yield cursor.fetch_next):
-                offer = cursor.next_object()
-                offers.append(offer)
-        elif search is not None:
-            # do search
-            search_terms = [re.compile(".*"+term.strip()+".*") for term in search.split(",")]
-            ret["search_terms"] = [term.strip() for term in search.split(",")]
-            cursor = db.offers.find({
-                "keywords.keyword": {
-                    "$in": search_terms
-                    }
-                })
-            while (yield cursor.fetch_next):
-                offer = cursor.next_object()
-                offers.append(offer)
-        elif uid is not None:
-            # is valid user id?
-            try:
-                user_id = ObjectId(uid)
-            except InvalidId:
-                self.send_error(400)
-                return
-            cursor = db.offers.find({
-                "candidates": user_id
-            })
-            while (yield cursor.fetch_next):
-                offer = cursor.next_object()
-                offers.append(offer)
-            ret["search_terms"] = [user_id]
+        # check offer id
+        try:
+            offer_id = ObjectId(offer_id)
+        except InvalidId:
+            self.send_error(400)
+            return
+
+        # do search
+        offer = yield db.offers.find_one({"_id": offer_id})
 
         # return offers
-        ret["result"] = offers
         self.set_status(200)
         self.set_header('Content-Type', 'application/json')
-        self.write(json.dumps(ret, default=jsonhandler.jsonhandler))
+        self.write(json.dumps(offer, default=jsonhandler.jsonhandler))
         self.finish()
 
     @gen.coroutine
